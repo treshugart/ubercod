e982543a3b769ecdc340117122a9e37a22979f51
Trey Shugart <treshugart@gmail.com>
2015-03-05 16:02:06 +1100

    Initial source import.

---
+++ index.js
@@ -0,0 +1,137 @@
+'use strict';
+
+var regexArgComments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var regexArgNames = /([^\s,]+)/g;
+var regexSingleton = /^\$/;
+var regexCtor = /^[A-Z]*/;
+
+function parseArgsFromFunc (func) {
+  var fnStr = func
+    .toString()
+    .replace(regexArgComments, '');
+
+  var result = fnStr
+    .slice(fnStr.indexOf('(')+1, fnStr.indexOf(')'))
+    .match(regexArgNames);
+
+  if (result === null) {
+    result = [];
+  }
+
+  return result;
+}
+
+function parseDepsFromFunc (func) {
+  return parseArgsFromFunc(func);
+}
+
+function applyCtor (Ctor, args) {
+  function NewCtor() {
+    return Ctor.apply(this, args) || this;
+  }
+  NewCtor.prototype = Ctor.prototype;
+  return new NewCtor();
+}
+
+function register (that, name, func) {
+  var cache;
+  var deps;
+  var isCtor;
+  var isSingleton;
+
+  // If the function to passed in is not a function, it is wrapped in a
+  // function that returns the value. This allows any value other than a
+  // function to be passed as a dependency and everything behaves normally.
+  function ensureFunc (funcToCheck) {
+    if (typeof funcToCheck !== 'function') {
+      return function () {
+        return funcToCheck;
+      };
+    }
+
+    return funcToCheck;
+  }
+
+  // Returns the dependencies of the current dependency and merges in any
+  // supplied, named arguments. Arguments take precedence over dependencies
+  // in the container.
+  function getDeps (args) {
+    return deps.map(function (dep) {
+      if (args && args[dep]) {
+        return args[dep];
+      }
+
+      if (that[dep]) {
+        if (typeof that[dep] === 'function') {
+          return that[dep]();
+        } else {
+          return that[dep];
+        }
+      }
+    });
+  }
+
+  // Returns a dependency. It checks to see if it's a constructor function
+  // or a regular function and calls it accordingly.
+  function getInst (args) {
+    args = getDeps(args);
+    return isCtor ? applyCtor(func, args) : func.apply(process, args);
+  }
+
+  // Resolves the dependency based on if it's a singleton or transient
+  // dependency. Both forms allow arguments. If it's a singleton, then no
+  // arguments are allowed. If it's transient, named arguments are allowed.
+  function resolveInst (args) {
+    return isSingleton ? cache || (cache = getInst()) : getInst(args);
+  }
+
+  // Initialises or re-initialises the state of the dependency.
+  function init (func) {
+    func = ensureFunc(func);
+    cache = undefined;
+    deps = parseDepsFromFunc(func);
+    isCtor = regexCtor.test(name);
+    isSingleton = regexSingleton.test(name);
+  }
+
+  // Initialse variables for the dependency.
+  init(func);
+
+  // Removes the $ from the beginning of the name.
+  name = isSingleton ? name.substring(1) : name;
+
+  // Ensure if it is labelled as a constructor to make the first character
+  // lowercase because it will be called like a function.
+  name = name[0].toLowerCase() + name.substring(1);
+
+  // Defines a property that returns the dependency or if it's transient, a
+  // function that returns the dependency and allows you to pass named
+  // arguments. If setting, then it overwrites the dependency and reinitialises
+  // everything to reconfigure it for the new dependency.
+  Object.defineProperty(that, name, {
+    enumerable: true,
+
+    get: function () {
+      return isSingleton ? resolveInst() : function (args) {
+        return resolveInst(args);
+      };
+    },
+
+    set: init
+  });
+}
+
+module.exports = function (that, registry) {
+  if (registry === undefined) {
+    registry = that;
+    that = {};
+  }
+
+  for (var name in registry) {
+    if (Object.prototype.hasOwnProperty.call(registry, name)) {
+      register(that, name, registry[name]);
+    }
+  }
+
+  return that;
+};
